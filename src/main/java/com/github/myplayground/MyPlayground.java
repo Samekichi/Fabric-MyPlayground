93b54567668cb03df4b177ad4946bedc2ae1a77e
Samekichi <karthus20@icloud.com>
2020-09-14 04:11:10 +0800

    Create `first_item`.

--- src/main/java/com/github/myplayground/MyPlayground.java
+++ src/main/java/com/github/myplayground/MyPlayground.java
@@ -1,14 +1,21 @@
 package com.github.myplayground;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class MyPlayground implements ModInitializer {
+	// 创建新物品的实例
+	public static final Item First_Item = new Item(new Item.Settings().group(ItemGroup.MISC));
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
+		Registry.register(Registry.ITEM, new Identifier("tutorial", "first_item"), First_Item);
 		System.out.println("Hello Fabric world!");
 	}
 }
